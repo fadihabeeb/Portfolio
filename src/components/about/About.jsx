0544940a64e3345630aea6b0a8b9f0b2bd400b42
Fadi Habeeb <fadi2habeeb@gmail.com>
2021-06-11 20:38:03 +0300

    V1.110 Mobile

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,23 +4,23 @@ export default function Testimonials() {
     return (
         <div className="about" id="about">
             <div className="container">
-            
+
                 <div className="item">
                     <div className="left">
-                    {/* <img  src="assests/about-me2.png" alt=""/> */}
+                        {/* <img  src="assests/about-me2.png" alt=""/> */}
                     </div>
                     <div className="right">
-                    <div className="wrapper">
-                        <h1>ABOUT ME</h1>
-                        <span>I have always been very passionate about technology and the way it can help you significantly improve your life. From amazing hardware that perfectly blends into your life to smart software that you never knew you've always needed.</span>
-                                            
-                        <span>I am a driven individual with the ability to adapt to any situation and proven potential to grow self and others</span>
-                        <span>Software Engineering B.Sc, at Ort Braude College of Engineering.
-                        Excellent communication and interpersonal skills. A fast learner with strong time management and multi-tasking skills. Strong work ethic in a team or individual setting to drive product success and process efficiency. Strong troubleshooting and problem-solving skills with an analytical mindset.
+                        <div className="wrapper">
+                            <h1>ABOUT ME</h1>
+                            <span>I have always been very passionate about technology and the way it can help you significantly improve your life. From amazing hardware that perfectly blends into your life to smart software that you never knew you've always needed.</span>
 
-Programming for me isn't solely concerning developing Automotive ways in which to sustain the environment, however additionally a "logic-based creativity" that may encourage its maker to accomplish his internal arrangement of achievement</span>
+                            <span>I am a driven individual with the ability to adapt to any situation and proven potential to grow self and others.</span>
+                            <span>Software Engineering B.Sc, at Ort Braude College of Engineering.
+                            Excellent communication and interpersonal skills. A fast learner with strong time management and multi-tasking skills. Strong work ethic in a team or individual setting to drive product success and process efficiency. Strong troubleshooting and problem-solving skills with an analytical mindset.
 
-                    </div>
+Programming for me isn't solely concerning developing Automotive ways in which to sustain the environment, however additionally a "logic-based creativity" that may encourage its maker to accomplish his internal arrangement of achievement.</span>
+
+                        </div>
                     </div>
                 </div>
             </div>
